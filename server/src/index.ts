cd3e762b8b17483d519fdf3c5d91d7637d0553cb
Ayesha Siroy <58470134+reisaya123@users.noreply.github.com>
2025-11-29 01:16:47 +0800

    feat: setup better auth (#16)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth/auth.js";
 
 dotenv.config({ path: ".env.local", quiet: true });
 
@@ -8,9 +10,9 @@ const PORT = parseInt(process.env.PORT || "3000", 10);
 
 const URL = process.env.URL || `http://localhost:${PORT}`;
 
-app.get("/", (_, res) => {
-  res.send(`Server is running at ${URL}`);
-});
+app.all("/api/auth/*", toNodeHandler(auth));
+
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${URL}`);
