53e471041cab5b75beb52643681a830590880cd0
Ayesha Siroy <58470134+reisaya123@users.noreply.github.com>
2025-12-16 01:43:01 +0800

    chore: modify theme and fix scripts (#21)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
+import { theme } from './theme';
 
 function App() {
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <Router />
     </MantineProvider>
   )
