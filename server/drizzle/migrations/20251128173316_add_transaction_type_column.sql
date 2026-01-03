7d030adc202c2d1fdf30f2f6e4245261b0bb8478
ayesha <siroy.ayesha@gmail.com>
2026-01-03 18:07:17 +0800

    refactor(server-schema): update user, account, session, and transaction tables

--- server/drizzle/migrations/20251128173316_add_transaction_type_column.sql
+++
@@ -1,3 +0,0 @@
-CREATE TYPE "public"."transactionType" AS ENUM('income', 'expense');--> statement-breakpoint
-ALTER TABLE "transactions" ADD COLUMN "type" "transactionType" NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "income";
